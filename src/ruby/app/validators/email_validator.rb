3b37c5bd2a0440ae28a417c9fdd56ebbc988b338
gitrahulgit <vrahul200023@gmail.com>
2020-10-28 23:13:42 +0530

    Added the Email validation in Ruby

--- src/ruby/app/validators/email_validator.rb
+++ src/ruby/app/validators/email_validator.rb
@@ -1,5 +1,13 @@
 class EmailValidator
   def is_valid?(email)
-    true
+    uppercase = ('A'..'Z')
+    if email.match?('[a-z0-9]+[_a-z0-9\.-][a-z0-9]+@[a-z0-9-]+(\.[a-z0-9-]+)(\.[a-z]{2,4})')
+      if (!email.each_char.any?{ |char| uppercase.cover?(char) })
+        if (!email.start_with?('.') && !email.end_with?('.'))
+          return true
+        end
+      end 
+    end
+    return false
   end
 end
